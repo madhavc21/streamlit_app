def8a20a89d117ebdb0f9bd573a04f374ef35e61
madhavc21 <116375634+madhavc21@users.noreply.github.com>
2024-05-14 15:40:12 +0000

    t

--- home.py
+++ home.py
@@ -7,17 +7,16 @@ st.divider()
 
 # File uploader widget
 image_file = st.file_uploader("Choose images", accept_multiple_files=True, type=['jpg', 'png', 'jpeg'])
-os.path.abspath(image_file)
-if image_file is not None:
-    st.image(image_file)
-    with open(os.path.join("upload",image_file),"wb") as f: 
-      f.write(image_file.getbuffer())         
-    st.success("Saved File")
+
+# if image_file is not None:
+#     st.image(image_file)
+#     with open(os.path.join("upload",image_file),"wb") as f: 
+#       f.write(image_file.getbuffer())         
+#     st.success("Saved File")
 
 lst2 = ['kurta_men', 'frock', 'jeans', 'shoes', 'gowns', 'pants', 'hoodie', 'saree', 'leggings_and_salwars', 'women_kurta', 'blouse', 'lehenga']
 for class_name in lst2:
   s = str.capitalize(class_name.replace("_", " "))
   st.write(f"{s} \n")
-  for i in os.listdir("upload"):
-    img = Image.open(i)
-    st.image(img)
+  for i in image_file:
+    st.image(i)
