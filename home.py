f7e388291532aa3a9b80f8ab9a4a7c88301dc7ed
madhavc21 <116375634+madhavc21@users.noreply.github.com>
2024-05-14 15:08:43 +0000

    f

--- home.py
+++ home.py
@@ -9,7 +9,7 @@ if not os.path.exists('upload'):
     os.makedirs('upload')
 st.write(os.path.abspath('upload'))
 
-flag = True
+flag = False
 if flag == True:
   for i in os.listdir('upload'):
    os.remove(i)
