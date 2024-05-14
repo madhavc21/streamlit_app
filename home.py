7ee88a49098f16cf97d8877447a407e8138df1a3
madhavc21 <116375634+madhavc21@users.noreply.github.com>
2024-05-14 19:05:26 +0530

    Update home.py

--- home.py
+++ home.py
@@ -4,6 +4,6 @@ st.title("Virtual Wardrobe")
 st.divider()
 st.file_uploader("Upload new", type=None, accept_multiple_files=True)
 
-lst2 : ['kurta_men', 'frock', 'jeans', 'shoes', 'gowns', 'pants', 'hoodie', 'saree', 'leggings_and_salwars', 'women_kurta', 'blouse', 'lehenga']
+lst2 = ['kurta_men', 'frock', 'jeans', 'shoes', 'gowns', 'pants', 'hoodie', 'saree', 'leggings_and_salwars', 'women_kurta', 'blouse', 'lehenga']
 for class_name in lst2:
   st.write(f"{class_name} \n")
