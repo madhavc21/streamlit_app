d25d6df51443e21267d49ce98f563a213651fc8f
madhavc21 <116375634+madhavc21@users.noreply.github.com>
2024-05-14 18:53:30 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,3 +1,5 @@
 import streamlit as st
 
 st.title("Virtual Wardrobe")
+st.divider()
+st.file_uploader("Upload new", type=None, accept_multiple_files=True, key=ImgUpload, help="Upload a new item to your wardrobe", on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
