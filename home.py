006f5c9a0c0df1f25c359d88ffda451a2a2dc841
madhavc21 <116375634+madhavc21@users.noreply.github.com>
2024-05-14 14:46:15 +0000

    chsnge

--- home.py
+++ home.py
@@ -1,17 +1,33 @@
 import streamlit as st
+import os
 from PIL import Image
 
 st.title("Virtual Wardrobe")
 st.divider()
-upload = st.file_uploader("Upload new", type=None, accept_multiple_files=True)
-if upload is not None:
-  for image in upload:
-    images = Image.open(image)
-    images.save('/workspaces/streamlit_app/upload')
-# with open("/workspaces/streamlit_app/upload") as uploaded:
-#   uploaded = upload.read()
+
+if not os.path.exists('upload'):
+    os.makedirs('upload')
+# File uploader widget
+uploaded_images = st.file_uploader("Choose images", accept_multiple_files=True, type=['jpg', 'png', 'jpeg'])
+
+# Save uploaded files to 'upload' directory
+for uploaded_image in uploaded_images:
+    if uploaded_image is not None:
+        # Construct the file path
+        file_path = os.path.join('upload', uploaded_image.name)
+        
+        # Write the file to the specified directory
+        with open(file_path, "wb") as f:
+            f.write(uploaded_image.getbuffer())
+        st.success(f"Saved File: {uploaded_image.name} to upload directory")
+
+# Accessing files from 'upload' directory
+# You can list the directory contents and do further processing as needed
+for filename in os.listdir('upload'):
+      st.write(filename)
+
 lst2 = ['kurta_men', 'frock', 'jeans', 'shoes', 'gowns', 'pants', 'hoodie', 'saree', 'leggings_and_salwars', 'women_kurta', 'blouse', 'lehenga']
 for class_name in lst2:
   s = str.capitalize(class_name.replace("_", " "))
   st.write(f"{s} \n")
-  st.image(upload) 
+  st.image(uploads) 
