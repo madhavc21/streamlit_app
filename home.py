8fbdcab3e2006f6707b856ca914db8bd0ecd28c9
madhavc21 <116375634+madhavc21@users.noreply.github.com>
2024-05-14 19:11:08 +0530

    Update home.py

--- home.py
+++ home.py
@@ -7,4 +7,4 @@ st.file_uploader("Upload new", type=None, accept_multiple_files=True)
 lst2 = ['kurta_men', 'frock', 'jeans', 'shoes', 'gowns', 'pants', 'hoodie', 'saree', 'leggings_and_salwars', 'women_kurta', 'blouse', 'lehenga']
 for class_name in lst2:
   s = str.capitalize(class_name.replace("_", " "))
-  st.write(f"{class_name} \n")
+  st.write(f"{s} \n")
